28eee65e61cc809a1192b0af2136425cfe5fd71f
Aiirey <xchomish@gmail.com>
2024-06-01 16:05:25 +0200

    BANANANANANANNAA-batman

--- bank/schema.sql
+++ bank/schema.sql
@@ -1,48 +1,47 @@
 \i schema_drop.sql
 
-CREATE TABLE IF NOT EXISTS User(
-	ID integer IDENTITY[(1,1)] PRIMARY KEY,
-	_name varchar(100) UNIQUE,
-	_password varchar(100)
+CREATE TABLE IF NOT EXISTS Users(
+	ID serial PRIMARY KEY,
+	name_ varchar(100) UNIQUE,
+	password_ varchar(100)
 );
 
 CREATE TABLE IF NOT EXISTS Supplier(
-	ID integer IDENTITY[(1,1)] PRIMARY KEY,
-	_name varchar(100) UNIQUE
+	ID serial PRIMARY KEY,
+	name_ varchar(100) UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS Category(
+	ID serial PRIMARY KEY,
+	name_ varchar(100),
+	supercategory_id integer DEFAULT NULL,
+	FOREIGN KEY (supercategory_id) REFERENCES Category(ID)
 );
 
 CREATE TABLE IF NOT EXISTS Item(
-	ID integer IDENTITY[(1,1)] PRIMARY KEY,
-	_name varchar(100) UNIQUE,
+	ID serial PRIMARY KEY,
+	name_ varchar(100) UNIQUE,
 	amount integer,
 	resaleprice integer,
 	category_id integer NOT NULL,
 	FOREIGN KEY (category_id) REFERENCES Category(ID)
 );
 
-CREATE TABLE IF NOT EXISTS Category(
-	ID integer IDENTITY[(1,1)] PRIMARY KEY,
-	_name varchar(100)
-	supercategory_id integer,
-	FOREIGN KEY (supercategory_id) REFERENCES Category(ID)
-);
-
 CREATE TABLE IF NOT EXISTS Delivers(
-	supplier integer,
-	item integer,
+	supplier_id integer,
+	item_id integer,
 	supplierprice integer,
-	CONSTRAINT delivers_key PRIMARY KEY (supplier, item),
-	FOREIGN KEY (supplier) REFERENCES Supplier(ID),
-	FOREIGN KEY (item) REFERENCES Item(ID)
+	CONSTRAINT delivers_key PRIMARY KEY (supplier_id, item_id),
+	FOREIGN KEY (supplier_id) REFERENCES Supplier(ID),
+	FOREIGN KEY (item_id) REFERENCES Item(ID)
 );
 
 CREATE TABLE IF NOT EXISTS Updates(
-	ID integer IDENTITY[(1,1)] PRIMARY KEY,
-	user integer,
-	item integer,
-	_timestamp TIMESTAMP, -- TODO: hvad fuck er et timestamp??
+	user_id integer,
+	item_id integer,
+	timestamp_ TIMESTAMP, -- TODO: hvad fuck er et timestamp??
 	change integer,
-	CONSTRAINT updates_key PRIMARY KEY (user, item),
-	FOREIGN KEY (user) REFERENCES User(ID),
-	FOREIGN KEY (item) REFERENCES Item(ID)
+	CONSTRAINT updates_key PRIMARY KEY (user_id, item_id),
+	FOREIGN KEY (user_id) REFERENCES Users(ID),
+	FOREIGN KEY (item_id) REFERENCES Item(ID)
 );
--- bank/schema_ins.sql
+++ bank/schema_ins.sql
@@ -2,16 +2,16 @@ DELETE FROM User;
 
 INSERT INTO User () VALUES ();
 
+INSERT INTO Category (name_) VALUES ('Sodavand');
 
-INSERT INTO Item (name, amount, resaleprice) VALUES ('Coca Cola', 3000, 10);
-INSERT INTO Item (name, amount, resaleprice) VALUES ('Squash', 1000, 10);
-
-
-INSERT INTO Supplier (name) ("Carlsberg")
-INSERT INTO Supplier (name) ("Drinx")
-
-INSERT INTO User (username, password) ("Admin", "1235")
+INSERT INTO Item (name_, amount, resaleprice, category_id) VALUES ('Coca Cola', 3000, 10, 1);
+INSERT INTO Item (name_, amount, resaleprice, category_id) VALUES ('Squash', 1000, 10, 1);
 
 
-INSERT INTO Category (name) VALUES ('Sodavand')
-INSERT INTO Delivers (supplier, item, supplierprice) VALUES (1, 1, 7)
+INSERT INTO Supplier (name_) ("Carlsberg");
+INSERT INTO Supplier (name_) ("Drinx");
+
+INSERT INTO User (name_, password_) ("Admin", "1235");
+
+
+INSERT INTO Delivers (supplier, item, supplierprice) VALUES (1, 1, 7);
