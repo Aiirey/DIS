358e5b2d7e971a41186843bf5fee49830d448f93
Aiirey <xchomish@gmail.com>
2024-06-06 15:01:41 +0200

    regex for brugeroprettelse

---
+++ pob/regex.py
@@ -0,0 +1,11 @@
+import re
+
+
+def validate_username(username):
+    return re.fullmatch("[A-ZÆØÅ][A-ZÆØÅa-zæøå0-9_-]*", username) != None
+
+
+def validate_password(password):
+    return re.fullmatch(
+        "[^ ]*[0-9][^ ]*[^0-9A-ZÆØÅa-zæøå ][^ ]*|[^ ]*[^0-9A-ZÆØÅa-zæøå ][^ ]*[0-9][^ ]*",
+        password) != None
